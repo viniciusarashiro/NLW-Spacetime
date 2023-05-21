0ea1d9537aef46d1f874e7243e0df1f8f91dcdbd
vinicius <viniciusarashiro0@gmail.com>
2023-05-21 00:19:36 -0300

    Aula 04

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,22 @@
 import 'dotenv/config'
 
 import fastify from 'fastify'
+import multipart from '@fastify/multipart'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true, // todas URLs de front-end poderão acessar nosso back-end
@@ -17,6 +27,7 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
